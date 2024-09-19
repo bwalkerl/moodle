cc10bd1412d27346ae97d7742cdb0f2821fd02be
Benjamin Walker <benjaminwalker@catalyst-au.net>
2024-09-19 10:49:20 +1000

    MDL-72133 report_log: Display logs filtered by deleted course ids

--- report/log/classes/renderer.php
+++ report/log/classes/renderer.php
@@ -93,7 +93,7 @@ class report_log_renderer extends plugin_renderer_base {
         echo html_writer::empty_tag('input', array('type' => 'hidden', 'name' => 'showcourses',
             'value' => $reportlog->showcourses));
 
-        $selectedcourseid = empty($reportlog->course) ? 0 : $reportlog->course->id;
+        $selectedcourseid = empty($reportlog->course) ? 0 : ($reportlog->sitecoursefilter ?? $reportlog->course->id);
 
         // Add course selector.
         $sitecontext = context_system::instance();

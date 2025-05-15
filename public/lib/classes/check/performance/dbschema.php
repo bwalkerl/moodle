30acccda24409fc91166ae8bd03a51c84f178708
Benjamin Walker <benjaminwalker@catalyst-au.net>
2025-05-15 17:21:50 +1000

    MDL-66827 report_performance: Convert schema alignment check into table

--- public/lib/classes/check/performance/dbschema.php
+++ public/lib/classes/check/performance/dbschema.php
@@ -63,23 +63,97 @@ class dbschema extends check {
      * @return result
      */
     public function get_result(): result {
-        global $DB;
+        global $DB, $PAGE;
 
         $dbmanager = $DB->get_manager();
         $schema = $dbmanager->get_install_xml_schema();
 
-        if (!$errors = $dbmanager->check_database_schema($schema)) {
+        if (!$errors = $dbmanager->check_database_schema($schema, null, true)) {
             return new result(result::OK, get_string('check_dbschema_ok', 'report_performance'), '');
         }
 
-        $details = '';
-        foreach ($errors as $tablename => $items) {
-            $details .= \html_writer::tag('h4', $tablename);
-            foreach ($items as $item) {
-                $details .= \html_writer::tag('pre', $item);
+        $checkrisky = optional_param('checkrisky', null, PARAM_BOOL);
+        if (isset($checkrisky)) {
+            $dbmanager->evaluate_risky_errors($errors);
+        }
+
+        ksort($errors);
+        $status = $this->get_status($errors);
+        $details = $this->get_table($errors);
+
+        $risky = $dbmanager->filter_errors($errors, '', [\database_manager::RISKY]);
+        if (!empty($risky)) {
+            $details .= \html_writer::link(
+                new \moodle_url($PAGE->url, ['detail' => 'core_dbschema', 'checkrisky' => true]),
+                get_string('check_dbschema_risky', 'report_performance'),
+                ['class' => 'btn btn-primary mt-2']
+            );
+        }
+
+        return new result($status, get_string('check_dbschema_errors', 'report_performance'), $details);
+    }
+
+    /**
+     * Gets the highest status found in the database schema errors
+     *
+     * @param array $errors Database schema errors
+     * @return string Higest status
+     */
+    public function get_status(array $errors): string {
+        $statuses = [];
+        foreach ($errors as $details) {
+            foreach ($details as $error) {
+                $statuses[] = $error->status;
             }
         }
-        return new result(result::ERROR, get_string('check_dbschema_errors', 'report_performance'), $details);
+        return result::get_highest_status($statuses) ?? result::ERROR;
+    }
+
+    /**
+     * Renders a html table of database schema errors
+     *
+     * @param array $errors Database schema errors
+     * @return string html table
+     */
+    public function get_table(array $errors): string {
+        global $OUTPUT;
+
+        $table = new \html_table();
+        $table->data = [];
+        $table->head = [
+            get_string('table', 'tool_xmldb'),
+            get_string('status'),
+            get_string('issue', 'report_performance'),
+            get_string('update'),
+            get_string('summary'),
+        ];
+        $table->id = 'dbschema_table';
+        $table->attributes = ['class' => 'admintable generaltable table-sm'];
+
+        foreach ($errors as $tablename => $details) {
+            $name = new \html_table_cell($tablename);
+            $name->rowspan = count($details);
+            $name->attributes['class'] = 'font-weight-bold';
+
+            foreach ($details as $index => $error) {
+                $row = [];
+                if ($index === 0) {
+                    $row[] = $name;
+                }
+
+                $result = new \html_table_cell($OUTPUT->check_result(new result($error->status, '', '')));
+                $result->attributes['class'] = 'status';
+                $row[] = $result;
+
+                $row[] = $error->type;
+                $row[] = $error->safety;
+                $row[] = $error->desc;
+
+                $table->data[] = $row;
+            }
+        }
+
+        return \html_writer::table($table);
     }
 }
 
